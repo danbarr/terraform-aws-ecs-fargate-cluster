2a75e631ddc3fcccc3fd3790c43e9534d27db079
Dan Barr <dan@dbarr.com>
2023-10-26 00:26:40 -0400

    Fix module outputs

--- outputs.tf
+++ outputs.tf
@@ -3,12 +3,7 @@ output "cluster_arn" {
   value       = aws_ecs_cluster.this.arn
 }
 
-output "cluster_id" {
-  description = "ID that identifies the cluster"
-  value       = aws_ecs_cluster.this.arn
-}
-
 output "cluster_name" {
   description = "Name that identifies the cluster"
-  value       = aws_ecs_cluster.this.arn
+  value       = aws_ecs_cluster.this.name
 }
